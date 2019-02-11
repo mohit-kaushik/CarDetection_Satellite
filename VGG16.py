5c2401388883703e7316b3f5c04e60afb129f48c
mohit <mohitkaushik2468@gmail.com>
2019-02-11 20:13:13 +0530

    VGG implemented

---
+++ VGG16.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from keras.layers import Dense,Flatten,Conv2D,Activation
+from keras.models import Sequential
+from keras.layers import MaxPool2D
+
+def VGG16():
+    model = Sequential()
+    model.add(Conv2D(input_shape=(61,61,4),filters=64,kernel_size=(3,3),padding="same", activation="relu"))
+    model.add(Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    
+    model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    
+    model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    
+    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    
+    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    
+    model.add(Flatten(name='flatten'))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    return model
