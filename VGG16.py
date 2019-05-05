44e037ae9933a16a7c81698d0177b09b9b7e66b6
mohit <mohitkaushik2468@gmail.com>
2019-05-05 18:41:17 +0530

    VGG16 implementation started

--- VGG16.py
+++ VGG16.py
@@ -5,31 +5,5 @@ from keras.layers import MaxPool2D
 
 def VGG16():
     model = Sequential()
-    model.add(Conv2D(input_shape=(61,61,4),filters=64,kernel_size=(3,3),padding="same", activation="relu"))
-    model.add(Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="relu"))
-    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
     
-    model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
-    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-    
-    model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-    
-    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-    
-    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-    
-    model.add(Flatten(name='flatten'))
-    model.add(Dense(256, activation='relu'))
-    model.add(Dense(128, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
     return model
