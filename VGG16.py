45eb80af6cda3fd60929e23854955bace8fc23f5
mohit <mohitkaushik2468@gmail.com>
2019-05-09 18:42:00 +0530

    VGG16 implementation block1

--- VGG16.py
+++ VGG16.py
@@ -5,5 +5,8 @@ from keras.layers import MaxPool2D
 
 def VGG16():
     model = Sequential()
+    model.add(Conv2D(input_shape=(61,61,4),filters=64,kernel_size=(3,3),padding="same", activation="relu"))
+    model.add(Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
     
     return model
