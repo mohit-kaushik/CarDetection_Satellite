b06b343e15e65b218b061f6aca513fb18c9287ad
mohit <mohitkaushik2468@gmail.com>
2019-05-21 18:42:42 +0530

    VGG16 implementation block1

--- VGG16.py
+++ VGG16.py
@@ -9,4 +9,8 @@ def VGG16():
     model.add(Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="relu"))
     model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
     
+    model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    
     return model
