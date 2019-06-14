8c40e95e9825789e3a6ba1fde01a730c586b698f
mohit <mohitkaushik2468@gmail.com>
2019-06-14 18:45:38 +0530

    VGG16 implementation block1

--- VGG16.py
+++ VGG16.py
@@ -28,4 +28,9 @@ def VGG16():
     model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
     model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
     
+    model.add(Flatten(name='flatten'))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    
     return model
