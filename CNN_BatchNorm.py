e4af3bdddc7dc9210d9fdf760a40fb9e46167815
mohit <mohitkaushik2468@gmail.com>
2019-07-29 18:57:23 +0530

    New model with batch norm

--- CNN_BatchNorm.py
+++ CNN_BatchNorm.py
@@ -8,4 +8,9 @@ def CNNModel():
     model.add(Conv2D(input_shape=(61,61,4),filters=64,kernel_size=(5,5),padding="same"))
     model.add(BatchNormalization())
     model.add(Activation("relu"))
+
+    model.add(Conv2D(filters=128, kernel_size=(5,5), padding="valid", activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+
     return model
